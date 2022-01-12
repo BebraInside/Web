2ecfe1ad28fdf43afadca6249efa6410d559abaa
Константин Харченко <kharshenko.kos@gmail.com>
2022-01-12 17:06:36 +0300

    Миграция на vue

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+    <div class="footer-text">
+      © 2021 Долгополова Вера
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped src="../assets/styles/footer_style.css">
+</style>
